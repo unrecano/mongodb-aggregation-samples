06f8703853ba0ad561222c2085e82fa3dff18f24
unrecano@diane <unrecano@gmail.com>
2026-04-18 10:26:54 -0500

    initial commit & match

---
+++ queries/query_match.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+client = MongoClient(
+    "mongodb://admin:test@localhost:27017/?authSource=admin&readPreference=primary&ssl=false"
+)
+result = client["sample_airbnb"]["listingsAndReviews"].aggregate(
+    [
+        {"$addFields": {"address.country_code": "US"}},
+        {"$sort": {"price": 1}},
+        {"$limit": 5},
+    ]
+)
+
+print(result.to_list())
